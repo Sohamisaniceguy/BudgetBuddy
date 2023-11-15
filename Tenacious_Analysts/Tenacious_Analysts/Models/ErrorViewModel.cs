dda96be144cc4d4e1aa5bf6b7bf2951e910ceba1
Soham <soham.np12@gmail.com>
2023-11-14 20:36:44 -0500

    Implemented Phase5

--- Tenacious_Analysts/Tenacious_Analysts/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Tenacious_Analysts.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
